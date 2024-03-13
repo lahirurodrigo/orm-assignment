5d7ea96b5d561d041c54aca283ba6f52e022f62e
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-03-13 12:22:47 +0530

    create sign in form

---
+++ src/main/java/lk/ijse/controller/SigninFormController.java
@@ -0,0 +1,44 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class SigninFormController {
+
+    @FXML
+    private Label btnLoginPage;
+
+    @FXML
+    private JFXButton btnSignin;
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXTextField txtUsername;
+
+    @FXML
+    void btnLoginPageOnAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnSigninOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtUsernameOnAction(ActionEvent event) {
+
+    }
+
+}
