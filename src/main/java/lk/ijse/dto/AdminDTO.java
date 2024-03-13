809473c88832630200e75ccfb13cac217fec88bf
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-03-13 15:00:29 +0530

    create AdminDTO and MemberDTO

---
+++ src/main/java/lk/ijse/dto/AdminDTO.java
@@ -0,0 +1,50 @@
+package lk.ijse.dto;
+
+public class AdminDTO {
+
+    private String username;
+    private String password;
+    private String email;
+
+    public AdminDTO() {
+    }
+
+    public AdminDTO(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberDTO{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
