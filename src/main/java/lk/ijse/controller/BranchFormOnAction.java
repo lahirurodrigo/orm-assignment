9c7e460a9649c8442f80fad2a098c8d72712414f
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-03-14 12:02:26 +0530

    add branches part

---
+++ src/main/java/lk/ijse/controller/BranchFormOnAction.java
@@ -0,0 +1,58 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class BranchFormOnAction {
+
+    @FXML
+    private JFXButton btnAdd;
+
+    @FXML
+    private JFXButton btnUpdate;
+
+    @FXML
+    private JFXButton btnView;
+
+    @FXML
+    private JFXButton btndelete;
+
+    @FXML
+    private JFXTextField txtID;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtIDOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtNameOnAction(ActionEvent event) {
+
+    }
+
+}
